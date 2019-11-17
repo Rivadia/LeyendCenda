e5beea744b1365890267336ee199e660930b7715
Rivadia <rivadia@gmail.com>
2019-11-17 15:50:44 -0600

    Initial commit

---
+++ Assets/Practica1/Scripts/EnemigoB.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoB : MonoBehaviour
+{
+    public List<Enemigo> enemigo;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        enemigo.Add(new Enemigo(100, 50, "Hongito"));
+        enemigo.Add(new Enemigo(200, 500, "Momia"));
+        enemigo.Add(new Enemigo(50, 50, "Conejito"));
+        enemigo.Add(new Enemigo(100, 1000, "CuboMalo"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    float rot;
+    // Update is called once per frame
+    void Update()
+    {
+         rot += Input.GetAxis("Horizontal2")*2;
+        //rot = Mathf.Clamp(rot, -60,60);
+       
+
+        transform.rotation = Quaternion.Euler(0, rot, 0);
+
+        //if (rot <= -60||rot >= 60 )
+        //{
+        //    transform.rotation = Quaternion.LookRotation(Vector3.forward.normalized*Time.deltaTime);
+            
+        //}
+        //transform.Rotate (0, rot * 1f, 0);
+
+
+    }
+}
